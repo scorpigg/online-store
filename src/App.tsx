a8c7949b8a28b1499678454e9681709a6a50f971
Andrew <scorpigg@yandex.ru>
2022-12-18 23:42:28 +0400

    feat: rebase img to public, add font, small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="wrapper">
-      <div className="header">
-        <div className="header-left">
-          <svg />
-          <div className="header-info">
-            <h3>Carfun</h3>
-            <span>Buy your dream car</span>
-          </div>
-        </div>
-        <div className="header-right">
-          <svg />
-          <span>$1205</span>
-        </div>
-      </div>
-    </div>
+    <div>Hello</div>
+    // <div className="wrapper">
+    //   <div className="header">
+    //     <div className="header-left">
+    //       <svg />
+    //       <div className="header-info">
+    //         <h3>Carfun</h3>
+    //         <span>Buy your dream car</span>
+    //       </div>
+    //     </div>
+    //     <div className="header-right">
+    //       <svg />
+    //       <span>$1205</span>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
