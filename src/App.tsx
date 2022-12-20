a5aca15e5c3e4c6727c52ad7237cd57e4efd7126
Andrey <46046907+scorpigg@users.noreply.github.com>
2022-12-21 00:19:22 +0400

    Revert "feat: finish item panel with sort, view, search"

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Card } from './components/Card';
 import { Filter } from './components/Filter';
 import { products } from './carBase';
 import { Footer } from './components/footer';
-import SortList from './SortList';
 
 function App() {
   return (
@@ -13,22 +12,6 @@ function App() {
       <hr />
       <main>
         <Filter />
-        <div className="items">
-          <div className="items__panel">
-            <span className="items__title">All cars</span>
-            <div className="items__sort">
-              <SortList />
-            </div>
-            <div className="items__view">
-              <div className="view-list"></div>
-              <div className="view-table"></div>
-            </div>
-            <div className="search-block">
-              <img src="img/search.svg" alt="Search" />
-              <input placeholder="Search..." />
-            </div>
-          </div>
-        </div>
         <div className="cards">
           {products.map((car) => (
             <Card {...car} key={car.id} />
