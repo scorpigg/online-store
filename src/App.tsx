3a79d4e0d95ffeba99ef3b3744cfd5f5c10810af
Andrew <scorpigg@yandex.ru>
2022-12-21 01:36:51 +0400

    feat: added interactivity to elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Header } from './components/Header';
 import { Card } from './components/Card';
 import { Filter } from './components/Filter';
 import { products } from './carBase';
-import { Footer } from './components/footer';
-import SortList from './SortList';
+import { Footer } from './components/Footer';
+import SortList from './components/SortList';
 
 function App() {
   return (
@@ -13,26 +13,28 @@ function App() {
       <hr />
       <main>
         <Filter />
-        <div className="items">
-          <div className="items__panel">
-            <span className="items__title">All cars</span>
-            <div className="items__sort">
-              <SortList />
-            </div>
-            <div className="items__view">
-              <div className="view-list"></div>
-              <div className="view-table"></div>
-            </div>
-            <div className="search-block">
-              <img src="img/search.svg" alt="Search" />
-              <input placeholder="Search..." />
+        <div className="main-container">
+          <div className="items">
+            <div className="items__panel">
+              <span className="items__title">All cars</span>
+              <div className="items__sort">
+                <SortList />
+              </div>
+              <div className="items__view">
+                <div className="view-list"></div>
+                <div className="view-table"></div>
+              </div>
+              <div className="search-block">
+                <img src="img/search.svg" alt="Search" />
+                <input placeholder="Search..." />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="cards">
-          {products.map((car) => (
-            <Card {...car} key={car.id} />
-          ))}
+          <div className="cards">
+            {products.map((car) => (
+              <Card {...car} key={car.id} />
+            ))}
+          </div>
         </div>
       </main>
       <hr />
