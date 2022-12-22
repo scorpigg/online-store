f027def7c0fc1b587ca63668fe168655cd1f4767
Irina Migunova <ista.irina@gmail.com>
2022-12-22 14:00:42 +0300

    feat: header and footer in home and basket

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from './Header';
-import { Footer } from './Footer';
 
 export default function RootLayout() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <hr />
-      <Outlet />
-      <hr />
-      <Footer />
-    </div>
-  );
+  return <Outlet />;
 }
